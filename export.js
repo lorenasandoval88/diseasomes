9cbe55d9bcc25dd87b8f7e5805439fdb23ef64f6
lorenasandoval88 <lorenasandoval88@gmail.com>
2023-01-04 14:29:43 -0500

    added img workflow

--- export.js
+++ export.js
@@ -381,7 +381,8 @@ function ui(targetDiv=document.body) {
     <p>
 	Below you can select, and inspect, <b style="color:maroon">A)</b> the <a href='https://www.pgscatalog.org' target="_blank">PGS Catalog</a> entries with risk scores for a list of genomic variations; and <b style="color:maroon">B)</b> <a href="https://you.23andme.com/tools/data/download" target="_blank">Your 23andMe data download</a>. Once you have both (A) and (B), you can proceed to <b style="color:maroon">C)</b> to calculate your raw polygenic risk score for the trait targetted by the PGS entry.
     </p>
-    <hr>
+    <hr></hr>
+    <img src="PRS_workflow_img.PNG" alt="prs workflow">
     `
     // recall that PGS23 is only global to the module, it is not exported
     PGS23.divPGS = document.createElement('div');
