6ffb38806bb12d9704ff18a8662a8a2a7483a85f
lorenasandoval88 <lorenasandoval88@gmail.com>
2023-01-26 16:41:22 -0500

    fix 23 meta

--- pgs.js
+++ pgs.js
@@ -1,15 +1,17 @@
 console.log('pgs.js loaded')
 
-pgs = {date:Date()}
+pgs = {
+    date: Date()
+}
 
 
-pgs.loadScript=async(url)=>{
+pgs.loadScript = async (url) => {
     let s = document.createElement('script')
-    s.src=url
+    s.src = url
     return document.head.appendChild(s)
 }
 
-pgs.plotAllMatchByPos=async(data, div = document.createElement('plotAllMatchByPosDiv'))=>{
+pgs.plotAllMatchByPos = async (data, div = document.createElement('plotAllMatchByPosDiv')) => {
     div.style.height = '500px'
     const indChr = data.pgs.cols.indexOf('hm_chr')
     const indPos = data.pgs.cols.indexOf('hm_pos')
@@ -44,12 +46,12 @@ pgs.plotAllMatchByPos=async(data, div = document.createElement('plotAllMatchByPo
     pgs.Plotly.newPlot(div, [trace0], {
         //title:`${data.pgs.meta.trait_mapped}, PRS ${Math.round(data.PRS*1000)/1000}`
         title: `<i style="color:navy">${data.pgs.meta.trait_mapped} (PGP#${data.pgs.meta.pgs_id.replace(/^.*0+/,'')}), PRS ${Math.round(data.PRS*1000)/1000}</i>
-			  <br><a href="${'https://doi.org/' + PGS23.pgsObj.meta.citation.match(/doi\:.*$/)[0]}" target="_blank"style="font-size:x-small">${data.pgs.meta.citation}</a>`,
+			  <br><a href="${'https://doi.org/' + data.my23.meta.citation.match(/doi\:.*$/)[0]}" target="_blank"style="font-size:x-small">${data.pgs.meta.citation}</a>`,
         yaxis: {
             title: '<span style="font-size:medium">variant i sorted by chromosome and position</span>',
             linewidth: 1,
-                mirror: true,
-                rangemode: "tozero",
+            mirror: true,
+            rangemode: "tozero",
         },
         xaxis: {
             title: '<span style="font-size:large">βi</span><span style="font-size:medium">, effect size (or beta) of variant i</span>',
@@ -66,7 +68,7 @@ pgs.plotAllMatchByPos=async(data, div = document.createElement('plotAllMatchByPo
     //debugger
 }
 
-pgs.Match2=async(data, progressReport)=>{
+pgs.Match2 = async (data, progressReport) => {
     // extract harmonized data from PGS entry first
     const indChr = data.pgs.cols.indexOf('hm_chr')
     const indPos = data.pgs.cols.indexOf('hm_pos')
@@ -101,7 +103,7 @@ pgs.Match2=async(data, progressReport)=>{
             let calcRiskScore = []
             let aleles = []
             data.pgsMatchMy23 = dtMatch
-      
+
             // calculate Risk
             let logR = 0
             // log(0)=1
@@ -137,10 +139,10 @@ pgs.Match2=async(data, progressReport)=>{
             if (calcRiskScore.reduce((a, b) => Math.min(a, b)) == 0) { //&&(calcRiskScore.reduce((a,b)=>Math.max(a,b))<=1)){ // hazard ratios?
                 console.log('these are not betas :-(')
                 //document.getElementById('my23CalcTextArea').value += ` Found ${data.pgsMatchMy23.length} PGS matches to the 23andme report.`
-    
+
                 //document.getElementById('my23CalcTextArea').value += ` However, these don't look right, QAQC FAILED ! ... You could look for another entry for the same trait where betas pass QAQC, maybe give it a try at https://www.pgscatalog.org/search/?q=${data.pgs.meta.trait_mapped.replace(' ','+')}.`
                 //document.getElementById('plotRiskDiv').hidden = true
-               // document.getElementById('hidenCalc').hidden = false
+                // document.getElementById('hidenCalc').hidden = false
                 //plotHazardAllMatchByPos()
                 //plotHazardAllMatchByEffect()
                 //plotAllMatchByEffect()
@@ -148,59 +150,63 @@ pgs.Match2=async(data, progressReport)=>{
                 data.PRS = Math.exp(calcRiskScore.reduce((a, b) => a + b))
                 //document.getElementById('my23CalcTextArea').value += ` Polygenic Risk Score (PRS) = ${Math.round(data.PRS * 1000) / 1000}, calculated from ${data.pgsMatchMy23.length}/${data.dt.length} matches.`
                 //my23CalcTextArea.value+=` ${data.pgsMatchMy23.length} PGS matches to the 23andme report.`
-              //document.getElementById('plotRiskDiv').hidden = false
+                //document.getElementById('plotRiskDiv').hidden = false
                 //document.getElementById('hidenCalc').hidden = false
                 //ploting
                 // plotAllMatchByPos()
                 // plotAllMatchByEffect()
                 // plotSummarySnps()
             }
-           // document.querySelector('#buttonCalculateRisk').disabled = false
-          // document.querySelector('#buttonCalculateRisk').style.color = 'blue'
+            // document.querySelector('#buttonCalculateRisk').disabled = false
+            // document.querySelector('#buttonCalculateRisk').style.color = 'blue'
         }
     }
     funMatch()
-  return data
+    return data
 }
 
-pgs.loadScore=async(entry='PGS000004',build=37,range)=>{
+pgs.loadScore = async (entry = 'PGS000004', build = 37, range) => {
     let txt = ""
-    if(typeof(entry)=='number'){
+    if (typeof (entry) == 'number') {
         entry = entry.toString()
-        entry = "PGS000000".slice(0,-entry.length)+entry
+        entry = "PGS000000".slice(0, -entry.length) + entry
     }
     //console.log(entry)
     // https://ftp.ebi.ac.uk/pub/databases/spot/pgs/scores/PGS000004/ScoringFiles/Harmonized/PGS000004_hmPOS_GRCh37.txt.gz
     const url = `https://ftp.ebi.ac.uk/pub/databases/spot/pgs/scores/${entry}/ScoringFiles/Harmonized/${entry}_hmPOS_GRCh${build}.txt.gz`
-    if(range){
-        if(typeof(range)=='number'){
-            range=[0,range]
+    if (range) {
+        if (typeof (range) == 'number') {
+            range = [0, range]
         }
         //debugger
-        txt= pgs.pako.inflate(await (await fetch(url,{
-            headers:{
+        txt = pgs.pako.inflate(await (await fetch(url, {
+            headers: {
                 'content-type': 'multipart/byteranges',
                 'range': `bytes=${range.join('-')}`,
             }
-        })).arrayBuffer(),{to:'string'})
+        })).arrayBuffer(), {
+            to: 'string'
+        })
         //debugger
-    }else{
-        txt = pgs.pako.inflate(await (await fetch(url)).arrayBuffer(),{to:'string'})
+    } else {
+        txt = pgs.pako.inflate(await (await fetch(url)).arrayBuffer(), {
+            to: 'string'
+        })
     }
     return txt
 }
 
-pgs.getArrayBuffer=async(range=[0,1000],url='https://ftp.ncbi.nih.gov/snp/organisms/human_9606/VCF/00-All.vcf.gz')=>{
-    return await (await (fetch(url,{
+pgs.getArrayBuffer = async (range = [0, 1000], url = 'https://ftp.ncbi.nih.gov/snp/organisms/human_9606/VCF/00-All.vcf.gz') => {
+    return await (await (fetch(url, {
         headers: {
-                'content-type': 'multipart/byteranges',
-                'range': `bytes=${range.join('-')}`,
-            }
+            'content-type': 'multipart/byteranges',
+            'range': `bytes=${range.join('-')}`,
+        }
     }))).arrayBuffer()
 }
 
 // create PGS obj and data
-pgs.parsePGS=async(i = 4)=>{
+pgs.parsePGS = async (i = 4) => {
     let obj = {
         id: i
     }
@@ -245,79 +251,81 @@ pgs.parsePGS=async(i = 4)=>{
     return obj
 }
 
-pgs.textArea = async (entry='PGS000004',build=37,range=20000)=>{
+pgs.textArea = async (entry = 'PGS000004', build = 37, range = 20000) => {
     let ta = document.createElement('textarea'); //DOM.element('textarea');
     ta.value = 'loading, please wait ...'
-    ta.style="width:100%;color:lime;background-color:black;height:20em;font-size:small"
+    ta.style = "width:100%;color:lime;background-color:black;height:20em;font-size:small"
     // find file size
-    if(typeof(entry)=='number'){
+    if (typeof (entry) == 'number') {
         entry = entry.toString()
-        entry = "PGS000000".slice(0,-entry.length)+entry
+        entry = "PGS000000".slice(0, -entry.length) + entry
     }
-    let response = await fetch(`https://ftp.ebi.ac.uk/pub/databases/spot/pgs/scores/${entry}/ScoringFiles/Harmonized/${entry}_hmPOS_GRCh${build}.txt.gz`,{method:'HEAD'})
+    let response = await fetch(`https://ftp.ebi.ac.uk/pub/databases/spot/pgs/scores/${entry}/ScoringFiles/Harmonized/${entry}_hmPOS_GRCh${build}.txt.gz`, {
+        method: 'HEAD'
+    })
     let fz = response.headers.get('Content-Length')
-    pgs.loadScore(entry,build,range).then(txt=>{
-        if(txt.length>range){
+    pgs.loadScore(entry, build, range).then(txt => {
+        if (txt.length > range) {
             // find file size
-            txt = txt.replace(/\n[^\n]*$/,`\n... (total size ${fz})`)
+            txt = txt.replace(/\n[^\n]*$/, `\n... (total size ${fz})`)
         }
-        ta.value=txt
+        ta.value = txt
     })
     return ta;
 }
 
 //pgs.url='https://www.pgscatalog.org/rest/'
-pgs.url='https://script.google.com/macros/s/AKfycbw1lC7UPcj34J06v_HWACyFJAPSoDB7VMI-KWbpb0mfuh9wccHPPFdbMdxGlUeyqDFM/exec?'
+pgs.url = 'https://script.google.com/macros/s/AKfycbw1lC7UPcj34J06v_HWACyFJAPSoDB7VMI-KWbpb0mfuh9wccHPPFdbMdxGlUeyqDFM/exec?'
 
-pgs.get=async(q='score/PGS000004?format=json')=>{ // PGS API call
-    const url = pgs.url+encodeURIComponent(q)
+pgs.get = async (q = 'score/PGS000004?format=json') => { // PGS API call
+    const url = pgs.url + encodeURIComponent(q)
     //return (await fetch(url)).json()
     let y
-    if(pgs.localforage){
+    if (pgs.localforage) {
         y = await pgs.localforage.getItem(url)
     }
-    if(!y){
+    if (!y) {
         y = await (await fetch(url)).json()
-        pgs.localforage.setItem(url,y)
+        pgs.localforage.setItem(url, y)
     }
     return y
 }
 
-pgs.getAttr=async(id='PGS000004')=>{ // getting attributes of a PSG entry
+pgs.getAttr = async (id = 'PGS000004') => { // getting attributes of a PSG entry
     return await pgs.get(`score/${id}?format=json`)
 }
 
-pgs.getValues=async(id='PGS000004')=>{ // getting values of a PSG entry by parsing the PSG file
+pgs.getValues = async (id = 'PGS000004') => { // getting values of a PSG entry by parsing the PSG file
     return await pgs.parse(id)
 }
 
-pgs.score={}
+pgs.score = {}
 //pgs.score.all=async fetch(url='https://www.pgscatalog.org/rest/score/all')
 
 
-pgs.loadDependencies=function(){
-    pgs.loadScript("https://cdnjs.cloudflare.com/ajax/libs/pako/1.0.11/pako.min.js").then(s=>{
-        s.onload=function(){
-            pgs.pako=pako
+pgs.loadDependencies = function () {
+    pgs.loadScript("https://cdnjs.cloudflare.com/ajax/libs/pako/1.0.11/pako.min.js").then(s => {
+        s.onload = function () {
+            pgs.pako = pako
         }
     })
-    pgs.loadScript("https://cdnjs.cloudflare.com/ajax/libs/localforage/1.9.0/localforage.min.js").then(s=>{
-        s.onload=function(){
-            pgs.localforage=localforage
+    pgs.loadScript("https://cdnjs.cloudflare.com/ajax/libs/localforage/1.9.0/localforage.min.js").then(s => {
+        s.onload = function () {
+            pgs.localforage = localforage
         }
     })
-    pgs.loadScript("https://cdnjs.cloudflare.com/ajax/libs/plotly.js/1.33.1/plotly.min.j").then(s=>{
-        s.onload=function(){
-            pgs.Plotly=Plotly
+    pgs.loadScript("https://cdnjs.cloudflare.com/ajax/libs/plotly.js/1.33.1/plotly.min.j").then(s => {
+        s.onload = function () {
+            pgs.Plotly = Plotly
         }
     })
     // https://cdnjs.cloudflare.com/ajax/libs/localforage/1.9.0/localforage.min.js  
 }
 
-pgs.deblank=(txt)=>{
-    return txt.replace(/^[#\s]+/,'').replace(/\s+?/,'')
+pgs.deblank = (txt) => {
+    return txt.replace(/^[#\s]+/, '').replace(/\s+?/, '')
 }
-pgs.parse23 = async(txt, info)=>{
+pgs.parse23 = async (txt, info) => {
     // normally info is the file name
     let obj = {}
     let rows = txt.split(/[\r\n]+/g)
@@ -335,59 +343,66 @@ pgs.parse23 = async(txt, info)=>{
         r[4] = i
         return r
     })
+    // parse metadata
+    obj.meta.txt.filter(r => (r[1] != '#')).forEach(aa => {
+        aa = aa.slice(1).split('=')
+        obj.meta[aa[0]] = aa[1]
+        //debugger
+    })
     return obj
 }
 
-pgs.prs23textArea = async(txt)=>{
+pgs.prs23textArea = async (txt) => {
     let ta = document.createElement('textarea'); //DOM.element('textarea');
     ta.value = 'loading, please wait ...'
-    ta.style="width:100%;color:lime;background-color:black;height:20em;font-size:small"
+    ta.style = "width:100%;color:lime;background-color:black;height:20em;font-size:small"
     ta.value = txt
-  return ta;
+    return ta;
 }
 
-pgs.parse=async(txt)=>{
-    if(!txt){ // sample score file
-        txt=await pgs.loadScore('PGS000004')
+pgs.parse = async (txt) => {
+    if (!txt) { // sample score file
+        txt = await pgs.loadScore('PGS000004')
     }
-    if(txt.length<100){
-        txt=await pgs.loadScore(txt)
+    if (txt.length < 100) {
+        txt = await pgs.loadScore(txt)
     }
-    let arr = txt.split(/\n/).filter(x=>x.length>0) // remove empty rows
-    let y={info:pgs.deblank(arr[0])}
-    let parm=''
-    for(var i = 1;i<arr.length;i++){
-        if(arr[i][0]=='#'){
-            if(arr[i][1]=='#'){
-                parm=pgs.deblank(arr[i])
-                y[parm]={}
-            }else{
+    let arr = txt.split(/\n/).filter(x => x.length > 0) // remove empty rows
+    let y = {
+        info: pgs.deblank(arr[0])
+    }
+    let parm = ''
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i][0] == '#') {
+            if (arr[i][1] == '#') {
+                parm = pgs.deblank(arr[i])
+                y[parm] = {}
+            } else {
                 let av = pgs.deblank(arr[i]).split('=').map(pgs.deblank)
-                y[parm][av[0]]=av[1]
+                y[parm][av[0]] = av[1]
             }
 
             //console.log(i,arr[i])
-        }
-        else{
+        } else {
             //console.log(i)
             break
         }
     }
     //console.log(i,arr[i])
     y.fields = arr[i].split(/\t/g) // list
-    y.values = arr.slice(i+1).map(x=>x.split(/\t/g).map(xi=>parseFloat(xi)?parseFloat(xi):xi))
+    y.values = arr.slice(i + 1).map(x => x.split(/\t/g).map(xi => parseFloat(xi) ? parseFloat(xi) : xi))
     return y
 }
 
-pgs.info = async(id='PGS000004')=>{
+pgs.info = async (id = 'PGS000004') => {
     return await pgs.get(`score/${id}?format=json`)
 }
-pgs.getRsid = async(x = 'chr1:g.100880328A>T?fields=dbsnp.rsid')=>{
+pgs.getRsid = async (x = 'chr1:g.100880328A>T?fields=dbsnp.rsid') => {
     let url = 'https://myvariant.info/v1/variant/'
-    if(typeof(x)=='string'){
-        url+=decodeURIComponent(x)
-    }else{ // something like [1,100880328,"T","A"]
-        url+=`chr${x[0]}:g.${x[1]}${x[3]}>${x[2]}?fields=dbsnp.rsid`
+    if (typeof (x) == 'string') {
+        url += decodeURIComponent(x)
+    } else { // something like [1,100880328,"T","A"]
+        url += `chr${x[0]}:g.${x[1]}${x[3]}>${x[2]}?fields=dbsnp.rsid`
     }
     //return (await fetch(url)).json()
     let y = await (await fetch(url)).json()
@@ -396,27 +411,30 @@ pgs.getRsid = async(x = 'chr1:g.100880328A>T?fields=dbsnp.rsid')=>{
     //https://myvariant.info/v1/variant/
 }
 
-pgs.dtFrame2Array=(fields,values)=>{
+pgs.dtFrame2Array = (fields, values) => {
     // under development
 }
 
-pgs.ini=()=>{ // act on context, such as search parameters. Not called automatically here.
-    pgs.parms={}
-    if(location.search.length>3){
-        location.search.slice(1).split('&').map(x=>{aa=x.split('=');pgs.parms[aa[0]]=aa[1]})
+pgs.ini = () => { // act on context, such as search parameters. Not called automatically here.
+    pgs.parms = {}
+    if (location.search.length > 3) {
+        location.search.slice(1).split('&').map(x => {
+            aa = x.split('=');
+            pgs.parms[aa[0]] = aa[1]
+        })
     }
-    if(pgs.parms.id){
+    if (pgs.parms.id) {
         let el = document.getElementById('inputID')
         let bt = document.getElementById('retrieveButton')
-        if(el&&bt){
-            el.value="PGS000000".slice(0,-pgs.parms.id.length)+pgs.parms.id
+        if (el && bt) {
+            el.value = "PGS000000".slice(0, -pgs.parms.id.length) + pgs.parms.id
             bt.click()
         }
     }
 }
 
 
-pgs.pgsPlot=async(dt, cols)=>{
+pgs.pgsPlot = async (dt, cols) => {
 
     // display pgs scores as beta or odds ratio with rsids or chr and position on the x axis
     let oddsRatio = {};
@@ -513,67 +531,67 @@ pgs.pgsPlot=async(dt, cols)=>{
         height: 600,
         hovermode: 'closest', //plot_bgcolor: 'rgb(254, 247, 234)', 
 
-        annotations: [
-            {
-              xref: 'paper',
-              yref: 'paper',
-              x: 0,
-              y: 1.06,
-              xanchor: 'left',
-              yanchor: 'left',
-              text: 'OR > 1 ~ higher odds of the outcome',
-              font:{
-                family: 'Arial',
-                size: 12,
-                color: 'rgb(150,150,150)'
-              },
-              showarrow: false
+        annotations: [{
+                xref: 'paper',
+                yref: 'paper',
+                x: 0,
+                y: 1.06,
+                xanchor: 'left',
+                yanchor: 'left',
+                text: 'OR > 1 ~ higher odds of the outcome',
+                font: {
+                    family: 'Arial',
+                    size: 12,
+                    color: 'rgb(150,150,150)'
+                },
+                showarrow: false
             },
             {
-              xref: 'paper',
-              yref: 'paper',
-              x: 0,
-              y: 1.03,
-              xanchor: 'left',
-              yanchor: 'left',
-              text: 'OR = 1 ~ no association',
-              font:{
-                family: 'Arial',
-                size: 12,
-                color: 'rgb(150,150,150)'
-              },
-              showarrow: false
+                xref: 'paper',
+                yref: 'paper',
+                x: 0,
+                y: 1.03,
+                xanchor: 'left',
+                yanchor: 'left',
+                text: 'OR = 1 ~ no association',
+                font: {
+                    family: 'Arial',
+                    size: 12,
+                    color: 'rgb(150,150,150)'
+                },
+                showarrow: false
             },
             {
-              xref: 'paper',
-              yref: 'paper',
-              x: 0,
-              y: 0.995,
-              xanchor: 'left',
-              yanchor: 'left',
-              text: 'OR < 1 ~ lower odds of the outcome',
-              font:{
-                family: 'Arial',
-                size: 12,
-                color: 'rgb(150,150,150)'
-              },
-              showarrow: false
+                xref: 'paper',
+                yref: 'paper',
+                x: 0,
+                y: 0.995,
+                xanchor: 'left',
+                yanchor: 'left',
+                text: 'OR < 1 ~ lower odds of the outcome',
+                font: {
+                    family: 'Arial',
+                    size: 12,
+                    color: 'rgb(150,150,150)'
+                },
+                showarrow: false
             }
-          ]
+        ]
     };
     pgs.Plotly.newPlot(div, data, layout)
 }
 
-if(typeof(define)!="undefined"){
+if (typeof (define) != "undefined") {
     //define(pgs)
     define(['https://cdnjs.cloudflare.com/ajax/libs/plotly.js/1.33.1/plotly.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/pako/2.0.3/pako.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/localforage/1.9.0/localforage.min.js'],function(Plotly,pako,localforage){
+        'https://cdnjs.cloudflare.com/ajax/libs/pako/2.0.3/pako.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/localforage/1.9.0/localforage.min.js'
+    ], function (Plotly, pako, localforage) {
         pgs.Plotly = Plotly
         pgs.pako = pako
-        pgs.localforage=localforage
+        pgs.localforage = localforage
         return pgs
     })
-}else{
+} else {
     pgs.loadDependencies()
 }
