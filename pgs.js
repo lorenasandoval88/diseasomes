8fdbb071183b809dd82c651a1561ab7f310f7829
lorenasandoval88 <lorenasandoval88@gmail.com>
2023-01-30 10:41:24 -0500

    Update pgs.js

--- pgs.js
+++ pgs.js
@@ -9,6 +9,156 @@ pgs.loadScript=async(url)=>{
     return document.head.appendChild(s)
 }
 
+pgsPlot = function(data) {
+    let div = DOM.element("PGSdiv");
+
+    // display pgs scores as beta or odds ratio with rsids or chr and position on the x axis
+    let oddsRatio = {};
+    const rs_idx = data.pgs.cols.indexOf('hm_rsID')
+
+    if (data.pgs.dt[0][rs_idx] == '' || data.pgs.dt[0][rs_idx] == undefined) {
+        data.pgs.dt.forEach((row) => {
+            oddsRatio["chr_" + row[8] + "_pos_" + row[9]] = math.exp(row[4]);
+        })
+    } else {
+        data.pgs.dt.forEach((row) => {
+            oddsRatio[row[0]] = math.exp(row[4]);
+        })
+    }
+    //sort pgs variants by beta
+    let oddsRatioSorted = Object.entries(oddsRatio)
+        .sort(([, a], [, b]) => a - b)
+        .reduce((r, [k, v]) => ({
+            ...r,
+            [k]: v
+        }), {});
+
+    // use plotly to make odds ratio chart----
+    var trace1 = {
+        type: 'scatter',
+        x: Object.values(oddsRatioSorted), // odds ratios
+        y: Object.keys(oddsRatioSorted), // rsids
+        mode: 'markers',
+        name: 'legend1',
+        marker: {
+            color: 'navy',
+            line: {
+                color: 'navy',
+                width: 1,
+            },
+            symbol: 'circle',
+            size: 4
+        }
+    };
+    var dat = [trace1];
+    var layout = {
+        title: `Odds Ratios (OR) for PGS Variants`,
+        yaxis: {
+            title: `variant rsid/chromosome and position`,
+        },
+        xaxis: {
+            title: `variant odds ratio`,
+            showgrid: false,
+            showline: true,
+            linecolor: 'rgb(102, 102, 102)',
+            titlefont: {
+                font: {
+                    size: 10,
+                    color: 'rgb(204, 204, 204)'
+                }
+            },
+            tickfont: {
+                font: {
+                    size: 10,
+                    color: 'rgb(102, 102, 102)'
+                }
+            },
+            autotick: true,
+            dtick: 10,
+            ticks: 'outside',
+            tickcolor: 'rgb(102, 102, 102)'
+        },
+        margin: {
+            l: 190,
+            r: 40,
+            b: 50,
+            t: 80
+        },
+        legend: {
+            font: {
+                size: 10,
+            },
+            yanchor: 'middle',
+            xanchor: 'right'
+        },
+        shapes: [{
+            type: 'line',
+            x0: 1,
+            y0: 0,
+            x1: 1,
+            y1: Object.values(oddsRatio).length,
+            line: {
+                color: 'grey',
+                width: 1.5,
+                dash: 'dot'
+            }
+        }],
+        width: 600,
+        height: 600,
+        hovermode: 'closest', //plot_bgcolor: 'rgb(254, 247, 234)', 
+
+        annotations: [
+            {
+              xref: 'paper',
+              yref: 'paper',
+              x: 0,
+              y: 1.06,
+              xanchor: 'left',
+              yanchor: 'left',
+              text: 'OR > 1 ~ higher odds of the outcome',
+              font:{
+                family: 'Arial',
+                size: 12,
+                color: 'rgb(150,150,150)'
+              },
+              showarrow: false
+            },
+            {
+              xref: 'paper',
+              yref: 'paper',
+              x: 0,
+              y: 1.03,
+              xanchor: 'left',
+              yanchor: 'left',
+              text: 'OR = 1 ~ no association',
+              font:{
+                family: 'Arial',
+                size: 12,
+                color: 'rgb(150,150,150)'
+              },
+              showarrow: false
+            },
+            {
+              xref: 'paper',
+              yref: 'paper',
+              x: 0,
+              y: 0.995,
+              xanchor: 'left',
+              yanchor: 'left',
+              text: 'OR < 1 ~ lower odds of the outcome',
+              font:{
+                family: 'Arial',
+                size: 12,
+                color: 'rgb(150,150,150)'
+              },
+              showarrow: false
+            }
+          ]
+    };
+    pgs.Plotly.newPlot(div, dat, layout)
+  return div
+}
+
 pgs.plotAllMatchByPos=(data,div2)=>{ 
     const indEffect_allele = data.pgs.cols.indexOf('effect_allele')
     div2.style.height = '350px'
