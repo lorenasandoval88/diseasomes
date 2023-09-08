912bbca5c4c89d47175609df5edb9f93085facde
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2023-09-08 11:38:45 -0400

    edited github page

--- pgs23.js
+++ pgs23.js
@@ -1,5 +1,5 @@
 (async()=>{
-    pgs23 = await import('https://episphere.github.io/prs/export.js')
+    pgs23 = await import('https://github.com/lorenasandoval88/diseasomes/export.js')
     //pgs23 = await import('http://127.0.0.1:5501/export.js')
     if(typeof(define)!='undefined'){
         define(pgs23)
